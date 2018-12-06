d1349628dad23a465ad25b6873d00f4bf45cb97c
chrissierra <christopher.sierra@usach.cl>
2018-12-06 17:53:34 -0300

    asdf

--- resources/lang/vendor/voyager/en/theme.php
+++ resources/lang/vendor/voyager/en/theme.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'footer_copyright'  => 'Hecho con <i class = "voyager-heart"> </i> por',
-    'footer_copyright2' => 'Hecho con ron e incluso más ron',
+    'footer_copyright'  => 'Desarrollo realizado por grupo Sister',
+    'footer_copyright2' => ' Christopher Sierra ',
 ];
